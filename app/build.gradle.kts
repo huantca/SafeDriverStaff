5877b3843fd56694177f0fb1d7ecaa0c64340d86
hungnv <hungkoj9x@gmail.com>
2024-01-05 14:46:36 +0700

    safeNavigateWithArgs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,6 +82,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    //Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
     //AndroidX
     implementation("androidx.preference:preference-ktx:1.2.1")
     //Hilt
--- app/src/main/java/com/bkplus/callscreen/common/BaseFragment.kt
+++ app/src/main/java/com/bkplus/callscreen/common/BaseFragment.kt
@@ -8,6 +8,8 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.NavDirections
 import androidx.viewbinding.ViewBinding
 import timber.log.Timber
 
@@ -80,5 +82,10 @@ abstract class BaseFragment<T : ViewBinding> : Fragment() {
             }
         }
         return if (result.isNotEmpty() && result[0] == '_') result.substring(1) else result.toString()
+    }
+    fun NavController.safeNavigateWithArgs(direction: NavDirections, bundle: Bundle? = Bundle()) {
+        currentDestination?.getAction(direction.actionId)?.run {
+            navigate(direction.actionId, bundle)
+        }
     }
 }
