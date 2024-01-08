6134fdb6c43a03e6adf63f6e974a50266a15dc35
hungnv <hungkoj9x@gmail.com>
2024-01-08 11:21:14 +0700

    define package

--- app/src/main/java/com/bkplus/callscreen/MainActivity.kt
+++ app/src/main/java/com/bkplus/callscreen/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.bkplus.callscreen
 
+import androidx.activity.viewModels
 import com.bkplus.callscreen.common.BaseActivity
 import com.harrison.myapplication.R
 import com.harrison.myapplication.databinding.ActivityScreenCallBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityScreenCallBinding>() {
+    private val viewModel: SharedViewModel by viewModels()
     override val layoutId: Int
         get() = R.layout.activity_screen_call
 
