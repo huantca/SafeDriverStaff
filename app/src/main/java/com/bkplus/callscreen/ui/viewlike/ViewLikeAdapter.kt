481e8b7acb9b0d66058487cc0917e1dabe567aee
khDai-172 <121807128+khDai-172@users.noreply.github.com>
2024-02-15 20:43:59 +0700

    set up adapter

--- app/src/main/java/com/bkplus/callscreen/ui/viewlike/ViewLikeAdapter.kt
+++ app/src/main/java/com/bkplus/callscreen/ui/viewlike/ViewLikeAdapter.kt
@@ -1,4 +1,19 @@
 package com.bkplus.callscreen.ui.viewlike
 
-class ViewLikeAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ViewLikeAdapter(private val type: String) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
