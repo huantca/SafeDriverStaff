c0683a35a9225e6f3d09a029cf4bd5aac3df9af4
khDai-172 <121807128+khDai-172@users.noreply.github.com>
2024-02-15 17:35:12 +0700

    setup view pager for view like container fragment

---
+++ app/src/main/java/com/bkplus/callscreen/ui/viewlike/ViewLikeAdapter.kt
@@ -0,0 +1,4 @@
+package com.bkplus.callscreen.ui.viewlike
+
+class ViewLikeAdapter {
+}
---
+++ app/src/main/java/com/bkplus/callscreen/ui/viewlike/WallPaper.kt
@@ -0,0 +1,6 @@
+package com.bkplus.callscreen.ui.viewlike
+
+data class WallPaper(
+    var isLiked: Boolean = false,
+    val imageUrl: String? = null
+)
