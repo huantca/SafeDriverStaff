bb5726208491673eb7b054c7c2c3805a66144a9c
hungnv <hungkoj9x@gmail.com>
2024-02-15 11:04:21 +0700

    BottomNavigationView

--- app/src/main/java/com/bkplus/callscreen/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/bkplus/callscreen/ui/setting/SettingFragment.kt
@@ -1,4 +1,10 @@
 package com.bkplus.callscreen.ui.setting
 
-class SettingFragment {
+import com.bkplus.callscreen.common.BaseFragment
+import com.harrison.myapplication.R
+import com.harrison.myapplication.databinding.FragmentSettingBinding
+
+class SettingFragment : BaseFragment<FragmentSettingBinding>() {
+    override val layoutId: Int
+        get() = R.layout.fragment_setting
 }
