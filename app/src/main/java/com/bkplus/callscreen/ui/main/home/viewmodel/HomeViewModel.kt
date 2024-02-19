16535aae095d097f1cd8af7013ec37d5977d584a
hajnam123 <awer134124@gmail.com>
2024-02-19 11:44:56 +0700

    history database intergrate

--- app/src/main/java/com/bkplus/callscreen/ui/main/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/bkplus/callscreen/ui/main/home/viewmodel/HomeViewModel.kt
@@ -5,21 +5,55 @@ import androidx.lifecycle.viewModelScope
 import com.bkplus.callscreen.api.ApiService
 import com.bkplus.callscreen.api.entity.HomeSectionEntity
 import com.bkplus.callscreen.api.onSuccess
+import com.bkplus.callscreen.database.WallpaperDao
+import com.bkplus.callscreen.database.WallpaperEntity
 import com.harison.core.app.utils.SingleLiveData
+import com.harrison.myapplication.BuildConfig
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val apiService: ApiService,
+    private val wallpaperDao: WallpaperDao
 ) : ViewModel() {
 
      val homeSectionLiveData = SingleLiveData<ArrayList<HomeSectionEntity>>()
+
+    init {
+        if (BuildConfig.DEBUG) fakeDataBase()
+    }
+
     fun getHomeSection(){
         viewModelScope.launch {
             apiService.getApiData().onSuccess {
                 homeSectionLiveData.postValue(it)
+            }
+        }
+    }
+
+    private fun fakeDataBase(){
+        viewModelScope.launch(Dispatchers.IO) {
+            Timber.tag("WallpaperDao").d(wallpaperDao.getCount().toString())
+            wallpaperDao.getCount().apply {
+                if (this == 0) {
+                    val list = arrayListOf(
+                        WallpaperEntity(generateId = 0,id = "1", imageUrl = "https://i.pinimg.com/736x/39/11/6c/39116c247669762f4ce72be4ce2b862e.jpg"),
+                        WallpaperEntity(generateId = 1,id = "2", imageUrl = "https://i.pinimg.com/736x/39/11/6c/39116c247669762f4ce72be4ce2b862e.jpg"),
+                        WallpaperEntity(generateId = 2,id = "3", imageUrl = "https://i.pinimg.com/736x/39/11/6c/39116c247669762f4ce72be4ce2b862e.jpg"),
+                        WallpaperEntity(generateId = 3,id = "4", imageUrl = "https://i.pinimg.com/736x/39/11/6c/39116c247669762f4ce72be4ce2b862e.jpg"),
+                        WallpaperEntity(generateId = 4,id = "5", imageUrl = "https://i.pinimg.com/736x/39/11/6c/39116c247669762f4ce72be4ce2b862e.jpg"),
+                        WallpaperEntity(generateId = 5,id = "6", imageUrl = "https://i.pinimg.com/736x/39/11/6c/39116c247669762f4ce72be4ce2b862e.jpg"),
+                        WallpaperEntity(generateId = 6,id = "7", imageUrl = "https://i.pinimg.com/736x/39/11/6c/39116c247669762f4ce72be4ce2b862e.jpg"),
+                        WallpaperEntity(generateId = 7,id = "8", imageUrl = "https://i.pinimg.com/736x/39/11/6c/39116c247669762f4ce72be4ce2b862e.jpg"),
+                        WallpaperEntity(generateId = 8,id = "9", imageUrl = "https://i.pinimg.com/736x/39/11/6c/39116c247669762f4ce72be4ce2b862e.jpg"),
+                        WallpaperEntity(generateId = 9,id = "10", imageUrl = "https://i.pinimg.com/736x/39/11/6c/39116c247669762f4ce72be4ce2b862e.jpg"),
+                    )
+                    wallpaperDao.insertAll(list)
+                }
             }
         }
     }
