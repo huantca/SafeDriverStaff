8bfa1e37da884522755fa0121e11ead91c460b0a
hajnam123 <awer134124@gmail.com>
2024-02-20 13:47:26 +0700

    update search

--- app/src/main/java/com/bkplus/callscreen/ui/main/home/search/adapter/HashTagAdapter.kt
+++ app/src/main/java/com/bkplus/callscreen/ui/main/home/search/adapter/HashTagAdapter.kt
@@ -1,23 +1,23 @@
 package com.bkplus.callscreen.ui.main.home.search.adapter
 
+import com.bkplus.callscreen.api.entity.Category
 import com.harison.core.app.platform.BaseRecyclerViewAdapter
 import com.harrison.myapplication.R
 import com.harrison.myapplication.databinding.ItemSearchHashtagBinding
 
-class HashTagAdapter : BaseRecyclerViewAdapter<HashTag, ItemSearchHashtagBinding>() {
+class HashTagAdapter(val onClick: (String) -> Unit) :
+    BaseRecyclerViewAdapter<Category, ItemSearchHashtagBinding>() {
     override fun getLayoutId(viewType: Int): Int {
         return R.layout.item_search_hashtag
     }
 
     override fun onBindViewHolder(
-        holder: BaseViewHolder<ItemSearchHashtagBinding, HashTag>,
+        holder: BaseViewHolder<ItemSearchHashtagBinding, Category>,
         position: Int
     ) {
-
+        holder.binding.name.text = "#" + items[position].name
+        holder.binding.name.setOnClickListener {
+            items[position].name?.let { it1 -> onClick(it1) }
+        }
     }
-
 }
-
-data class HashTag(
-    val name: String
-)
--- app/src/main/java/com/bkplus/callscreen/ui/main/home/search/adapter/SearchAdapter.kt
+++ app/src/main/java/com/bkplus/callscreen/ui/main/home/search/adapter/SearchAdapter.kt
@@ -1,23 +1,20 @@
 package com.bkplus.callscreen.ui.main.home.search.adapter
 
+import com.bkplus.callscreen.api.entity.Item
+import com.bkplus.callscreen.ultis.loadImage
 import com.harison.core.app.platform.BaseRecyclerViewAdapter
 import com.harrison.myapplication.R
 import com.harrison.myapplication.databinding.ItemSearchBinding
 
-class SearchAdapter :BaseRecyclerViewAdapter<SearchItem, ItemSearchBinding>() {
+class SearchAdapter :BaseRecyclerViewAdapter<Item, ItemSearchBinding>() {
     override fun getLayoutId(viewType: Int): Int {
         return R.layout.item_search
     }
 
     override fun onBindViewHolder(
-        holder: BaseViewHolder<ItemSearchBinding, SearchItem>,
+        holder: BaseViewHolder<ItemSearchBinding, Item>,
         position: Int
     ) {
-
+        holder.binding.image.loadImage(items[position].url)
     }
-
 }
-
-data class SearchItem (
-    val url: String
-)
