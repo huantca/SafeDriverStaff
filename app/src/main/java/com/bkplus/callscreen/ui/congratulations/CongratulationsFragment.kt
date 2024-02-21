36f4b7d8b6eda24e0fbcdfdd0a84422f4a4b3c1e
BUI-XUAN-HUAN <89187491+huantca@users.noreply.github.com>
2024-02-21 11:00:03 +0700

    ads native congratulation

--- app/src/main/java/com/bkplus/callscreen/ui/congratulations/CongratulationsFragment.kt
+++ app/src/main/java/com/bkplus/callscreen/ui/congratulations/CongratulationsFragment.kt
@@ -1,8 +1,15 @@
 package com.bkplus.callscreen.ui.congratulations
 
+import android.view.View
 import androidx.navigation.fragment.findNavController
+import com.ads.bkplus_ads.core.callback.BkPlusNativeAdCallback
+import com.ads.bkplus_ads.core.callforward.BkPlusNativeAd
+import com.ads.bkplus_ads.core.model.BkNativeAd
 import com.bkplus.callscreen.common.BaseFragment
 import com.bkplus.callscreen.ultis.setOnSingleClickListener
+import com.bumptech.glide.Glide
+import com.google.android.gms.ads.LoadAdError
+import com.harrison.myapplication.BuildConfig
 import com.harrison.myapplication.R
 import com.harrison.myapplication.databinding.FragmentCongratulationsBinding
 
@@ -11,6 +18,14 @@ class CongratulationsFragment: BaseFragment<FragmentCongratulationsBinding>() {
     override val layoutId: Int
         get() = R.layout.fragment_congratulations
 
+    override fun setupUI() {
+        super.setupUI()
+        binding.apply {
+            Glide.with(this@CongratulationsFragment)
+                .load(R.drawable.gif_congratulation)
+                .into(congratulateGif)
+        }
+    }
     override fun setupListener() {
         super.setupListener()
         binding.apply {
@@ -23,4 +38,38 @@ class CongratulationsFragment: BaseFragment<FragmentCongratulationsBinding>() {
             }
         }
     }
+
+
+    private fun loadNativeAd() {
+        BkPlusNativeAd.loadNativeAd(
+            this,
+            BuildConfig.native_categories,
+            R.layout.layout_native_congratulation,
+            object : BkPlusNativeAdCallback() {
+                override fun onNativeAdLoaded(nativeAd: BkNativeAd) {
+                    super.onNativeAdLoaded(nativeAd)
+                    populateNativeAd(nativeAd)
+                }
+
+                override fun onAdFailedToLoad(error: LoadAdError) {
+                    super.onAdFailedToLoad(error)
+                    removeNativeAd()
+                }
+            }
+        )
+    }
+
+    private fun populateNativeAd(nativeAd: BkNativeAd) {
+        binding.shimmerContainerNative2.visibility = View.GONE
+        binding.shimmerContainerNative2.stopShimmer()
+        binding.flAdPlaceholderDeviceInfo.visibility = View.VISIBLE
+        BkPlusNativeAd.populateNativeAd(this, nativeAd, binding.flAdPlaceholderDeviceInfo)
+    }
+
+    private fun removeNativeAd() {
+        binding.shimmerContainerNative2.stopShimmer()
+        binding.shimmerContainerNative2.visibility = View.GONE
+        binding.frNativeAdsDeviceInfo.visibility = View.GONE
+    }
+
 }
