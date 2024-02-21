076e4181d70ecdce80aae42d48495041906766d6
khDai-172 <121807128+khDai-172@users.noreply.github.com>
2024-02-21 10:37:08 +0700

    refactoring

--- app/src/main/java/com/bkplus/callscreen/ui/main/home/favourite/FavouriteViewModel.kt
+++ app/src/main/java/com/bkplus/callscreen/ui/main/home/favourite/FavouriteViewModel.kt
@@ -3,28 +3,26 @@ package com.bkplus.callscreen.ui.main.home.favourite
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.bkplus.callscreen.api.ApiService
-import com.bkplus.callscreen.api.onSuccess
 import com.bkplus.callscreen.database.WallpaperDao
 import com.bkplus.callscreen.database.WallpaperEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class FavouriteViewModel @Inject constructor(
-    private val wallpaperDao: WallpaperDao,
-    private val apiService: ApiService
+    private val wallpaperDao: WallpaperDao
 ) : ViewModel() {
     val favouriteList = MutableLiveData<List<WallpaperEntity>>()
     fun getData() {
         viewModelScope.launch(Dispatchers.IO) {
-           wallpaperDao.getAll().collect {
-               val list = it.filter { item -> item.isLike == true }
-               favouriteList.postValue(list)
-           }
+            wallpaperDao.getAll().collect {
+                val list = it.filter { item -> item.isLike }
+                Timber.tag("Favourite Wallpaper").e("---- ${it.count()}")
+                favouriteList.postValue(list)
+            }
         }
     }
 }
