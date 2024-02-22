a524685ae64c5d5b78784cc58935276dc8a0ede7
hungnv <hungkoj9x@gmail.com>
2024-02-22 15:47:46 +0700

    update lib ads

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -182,7 +182,7 @@ dependencies {
     ksp("androidx.room:room-compiler:$room_version")
     implementation("androidx.room:room-ktx:$room_version")
     //BK Ads Lib
-    implementation("com.bkplus.ads:library:1.0.4-alpha08")
+    implementation("com.bkplus.ads:library:1.0.4-alpha09")
     //Appsflyer
     implementation("com.appsflyer:adrevenue:6.9.0")
     // https://mvnrepository.com/artifact/com.appsflyer/af-android-sdk
